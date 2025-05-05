5b5f352e417e08a74023682e7c6c3b70148938e5
momokaP <9451sh@naver.com>
2025-05-05 23:07:24 +0900

    collision check

--- CapStone/Weapon.h
+++ CapStone/Weapon.h
@@ -20,10 +20,6 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-	UFUNCTION()
-	void OnWeaponOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
-    UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
-
 private:
 	UPROPERTY(EditDefaultsOnly, Category = "Combat")
 	USkeletalMeshComponent* SkeletalMesh;
